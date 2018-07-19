f478e930c7770c9db2f2eae61c1f5341db4a87f3
caneballante <jonbridgman@gmail.com>
2018-07-19 10:59:48 -0700

    2018 NTA SUBMITTED
    added the draft text
    rearranged text so the table is up top
    added anchor links to the to top of text to access details

--- 2018_AA_submitted_NTA.php
+++ 2018_AA_submitted_NTA.php
@@ -68,38 +68,89 @@
 		</div>
 		 
 		<div class="col-sm-9 padding-20-top padding-40-right content-column mobile-padding-20-left"> <!-- InstanceBeginEditable name="9col_content" -->
-			<p>The development and submittal process for Near Term Actions (NTAs) for the 2018-2022 Action Agenda is now complete. 635 NTAs have been put forward to be reviewed and evaluated for adoption into the 2018-2022 Action Agenda. NTAs were received from a diversity of organizations, including federal, state, local, and tribal government agencies; non-governmental organizations; academic institutions; foundations; and private businesses.  NTAs address most of the Regional Priorities outlined in the NTA solicitation, and represent all regions of Puget Sound.			<br>
-The submitted NTAs are searchable by Vital Sign, Regional Priority, Local Integrating Organization area by using the search fields, as noted below.&nbsp;</p>
-			<p><br>
-				<a href="2018_AA_preregistration_map.php">A map of submitted NTAs is also available on our website.</a></p>
+		<p><strong>The review process for the 2018-2022 Action Agenda Near Term Actions (NTAs) is now complete! </strong><strong> </strong><br>
+635 NTAs were put forward to be reviewed and evaluated for adoption into the 2018-2022 Action Agenda. You can view them in the tool below.
+		<p><a href="#process">LEARN ABOUT THE PROCESS</a> | <a href="#procedural">NTA PROCEDURAL RECORDS</a> | <a href="#appeal">APPEALING AN NTA EVALUATION</a></p>
 			
-
-			<!-- Nav tabs -->
+<!-- Nav tabs -->
 <ul class="nav nav-tabs" role="tablist">
-  <li class="active"><a href="#full-list" role="tab" data-toggle="tab">Full list of preregistered NTAs</a></li>
-  <li><a href="#profile" role="tab" data-toggle="tab">Search list by Vital Signs and Regional Priorities</a></li>
-  <li><a href="#messages" role="tab" data-toggle="tab">Search list by Local Areas/LIOs</a></li>
+  <li class="active"><a href="#1" role="tab" data-toggle="tab" onClick="ga('send', 'event', 'ntaviewer', 'click', 'Recommended');">Recommended NTAs</a></li>
+  <li><a href="#2" role="tab" data-toggle="tab" onClick="ga('send', 'event', 'ntaviewer', 'click', 'Tier');">By Tier</a></li>
+  <li><a href="#3" role="tab" data-toggle="tab" onClick="ga('send', 'event', 'ntaviewer', 'click', 'Vital sign');">By Vital Sign</a></li>
+  <li><a href="#4" role="tab" data-toggle="tab" onClick="ga('send', 'event', 'ntaviewer', 'click', 'Owner');">By Owner</a></li>
+  <li><a href="#5" role="tab" data-toggle="tab" onClick="ga('send', 'event', 'ntaviewer', 'click', 'LIO');">by LIO</a></li>
+  <li><a href="#6" role="tab" data-toggle="tab" onClick="ga('send', 'event', 'ntaviewer', 'click', 'Priority');">By Regional Priority Approach</a></li>
+  <li><a href="#7" role="tab" data-toggle="tab"onClick="ga('send', 'event', 'ntaviewer', 'click', 'Submitted');">Submitted NTAs</a></li>
   
 </ul>
 
 <!-- Tab panes -->
 <div class="tab-content">
 	 <!--tab 1 -->
-  <div class="tab-pane active  iframe-actionagenda" id="full-list">	
-	<iframe src="https://c4esh335.caspio.com/dp/8ab3500078174df21f8a4ef58cd0" style="border:none;" width="100%" height="100% "allowfullscreen></iframe></div>
+	<div class="tab-pane active  iframe-actionagenda" id="1">	
+		<iframe name="Public Recommended Results" title="Public Recommended Results" src="https://c4esh335.caspio.com/dp/8ab35000d9d99e00958d4adf8238">Sorry, but your browser does not support frames.</iframe><br>
+	</div>
   <!--tab 2 -->
-	
-
-	<div class="tab-pane iframe-actionagenda" id="profile">
-		<iframe src="https://c4esh335.caspio.com/dp/8ab35000691df6fe41f14e1d9626" style="border:none;" width="100%" height="100% "allowfullscreen></iframe>
+	<div class="tab-pane iframe-actionagenda" id="2">
+		<iframe name="Public Recommended Results_tier" title="Public Recommended Results_tier" src="https://c4esh335.caspio.com/dp/8ab35000098cf792a37d48229a6b">Sorry, but your browser does not support frames.</iframe>
 	</div>
   <!--tab 3 -->
-	<div class="tab-pane iframe-actionagenda" id="messages">
-	<iframe src="https://c4esh335.caspio.com/dp/8ab350004b6a842ed48c440c94ca" style="border:none;" width="100%" height="100% "allowfullscreen></iframe>
+	<div class="tab-pane iframe-actionagenda" id="3">
+		<iframe name="Public Recommended Results_vs" title="Public Recommended Results_vs" src="https://c4esh335.caspio.com/dp/8ab35000c51dec93874042188f70">Sorry, but your browser does not support frames.</iframe>
+	</div>
+  <!--tab 4 -->
+	<div class="tab-pane iframe-actionagenda" id="4">
+		<iframe name="Public Recommended Results_org" title="Public Recommended Results_org" src="https://c4esh335.caspio.com/dp/8ab35000f6954f0c250a44da8ba5">Sorry, but your browser does not support frames.</iframe>
+	</div>
+  <!--tab 5 -->
+	<div class="tab-pane iframe-actionagenda" id="5">
+		<iframe name="Public Recommended Results_lio" title="Public Recommended Results_lio" src="https://c4esh335.caspio.com/dp/8ab3500032a31b6b5c3f47178344">Sorry, but your browser does not support frames.</iframe>
+	</div>
+  <!--tab 6 -->
+	<div class="tab-pane iframe-actionagenda" id="6">
+		<iframe name="Public Recommended Results_rpa" title="Public Recommended Results_rpa" src="https://c4esh335.caspio.com/dp/8ab35000ebe6b53259bf4fbdba75">Sorry, but your browser does not support frames.</iframe>
+	</div>
+  <!--tab 7 -->
+	<div class="tab-pane iframe-actionagenda" id="7">
+		<iframe name="Public Submitted List" title="Public Submitted List" src="https://c4esh335.caspio.com/dp/8ab3500078174df21f8a4ef58cd0">Sorry, but your browser does not support frames.</iframe>
 	</div>
 
+
 </div>
+			<div id="process"></div>
+			<h2>About the NTA prioritization Process</h2>
+			<p>NTAs were received from a diversity of organizations, including federal, state, local, and tribal government agencies; non-governmental organizations; academic institutions; foundations; and private businesses. NTAs address most of the <a href="https://pspwa.box.com/s/7t7c4eszegdqh5gu5koirndiy0nr96vc">Regional Priorities</a> outlined in the NTA <a href="https://pspwa.box.com/s/p847yyo5pfjm0dk59btk22ozmt5a2w1d">Solicitation</a>, and represent all regions of Puget Sound. All NTAs were reviewed through robust local and regional processes with habitat, shellfish, stormwater and salmon experts from across the Sound. </p>
+			<p>
+			NTAs were prioritized into four categories using the process and criteria outlined in the <a href="https://pspwa.box.com/s/p847yyo5pfjm0dk59btk22ozmt5a2w1d">Solicitation</a>.  The purpose of the prioritization was to group the NTAs relative to how much they are expected to advance recovery over the next 4 years. NTAs that are grouped into the following three categories (tiers) will be recommended for adoption into the 2018-2022 Action Agenda: outstanding (4), good (3), acceptable with revisions (2). Those NTAs that are grouped into category 1 (poor) will not be recommended for adoption. </p>
+			<p>The results of evaluations are posted in the tables below. Recommended NTAs are also presented and searchable by Vital Sign, owner organization, Local Integrating Organization (LIO), Regional Priority approach, and final tier. For each NTA, a summary from the regional and local evaluations are provided by selecting the final tier. </p>
+			<ul>
+				<li>For all NTAs: Scores from the regional Strategic Initiative Advisory Team review are provided for each evaluation criteria listed in the Solicitation. Justification is provided for any scores of 1 or 2. </li>
+				<li>For all NTAs that are proposed to be implemented in one or two LIO geographies: Final score from the relevant LIOs are provided. Justification is provided for any scores of 1 or 2.</li>
+				<li>For some NTAs that are proposed to be implemented in two or more LIO geographies: LIOs were provided an option to review those NTAs. Where an LIO review was conducted, scores for each evaluation criteria listed in the Solicitation is provided. Justification is provided for any scores of 1 or 2.</li>
+			</ul>
+			<p>All NTAs that received a final score of 1 are not included in this published list. The Partnership has sent individual emails to relevant NTA managers to notify them on the results. </p>
+			<div id="procedural"></div>
+			<h2>NTA Procedural Records </h2>
+			<p>Procedural records summarizing how the NTA development and review process were implemented are available at the links below: <br>
+			Access the procedural records from the Local Integrating Organizations review process. &lt;INSERT LINK&gt; <br>
+			Access the procedural record from the regional Strategic Initiative Advisory Team review process. &lt;INSERT LINK&gt; <br>
+			Access a full list of reviewers. &lt;INSERT LINK&gt; </p>
+			<div id="appeal"></div>
+		<h2>How can a NTA owner appeal the evaluation of their NTA? </h2>
+				<p>Any NTA manager who feels that their NTA was not fairly and consistently processed in accordance with the procedures stated in the <a href="https://pspwa.box.com/s/p847yyo5pfjm0dk59btk22ozmt5a2w1d">2018-2022 Action Agenda Solicitation for Near Term Actions</a> may submit a petition to the Partnership. </p>
+			<p>The period for submitting a petition will begin on Wednesday, August 1, 2018, and will close on Wednesday, August 15, 2018. <strong>To submit a petition, a petition form must be filled out and emailed to the Partnership by 5:00 pm (PST), Wednesday, August 15, 2018</strong>. <br>
+				Access the NTA Petition Form &lt;INSERT LINK&gt;. <br>
+				Petition forms should be emailed to:  <br>
+				            Heather Saunders Benson, Action Agenda Manager<br>
+				            Puget Sound Partnership <br>
+				            <a href="mailto:heather.benson@psp.wa.gov">heather.benson@psp.wa.gov</a> <br>
+				            360.464.2005  </p>
+			<p>Once a petition has been emailed to the Partnership, confirmation that the petition has been received the petition will be provided to the NTA manager via email. A committee of the chairs and vice-chairs of the Partnership&rsquo;s boards will review and adjudicate the petitions by Tuesday, August 24, 2018. The committee will review process concerns only, and not reevaluate the merits of an NTA.  <br>
+				If you have any questions about the petition process, please contact Heather Saunders Benson, Action Agenda Planning Manager, at <a href="mailto:heather.benson@psp.wa.gov">heather.benson@psp.wa.gov</a>, or by phone at 360.464.2005.</p>
 			
+	
+
+	
 			
 		
 		<!-- InstanceEndEditable --> </div>
